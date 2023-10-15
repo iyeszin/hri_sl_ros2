ef162dd7c0a6edf99bbea576bc627257eda4e568
iyeszin <iyeszin@gmail.com>
2023-10-15 14:42:49 +0800

    fix the launch terminal for gui

--- launch_gui.sh
+++ launch_gui.sh
@@ -3,4 +3,6 @@ colcon build
 . install/setup.bash
 
 # 2. Launch 
-ros2 launch rh8d_controller_python position_controller.launch.py 
+ros2 launch rh8d_hw rh8d_hw_combined.launch.py
+ros2 launch rh8d_ros2 rh8d_col.launch.py
+ros2 launch rh8d_controller_python controller_gui.launch.py

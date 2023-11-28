e12ac50bb16c1beb63a262181f597c1d7ae298a1
Iye Szin <iyeszin@gmail.com>
2023-11-28 16:20:54 +0100

    fix the shell scripts

--- launch_all.sh
+++ launch_all.sh
@@ -1,10 +1,12 @@
-# 1. Build the ROS 2 workspace:
-colcon build
-. install/setup.bash
+#!/bin/bash
 
-# 2. Launch the required components in separate terminals:
-ros2 launch rh8d_controller_python position_controller.launch.py
-ros2 launch rh8d_hw rh8d_hw_combined.launch.py
-ros2 launch rh8d_ros2 rh8d_col.launch.py
-ros2 launch ros_sign_language_recognition recognizer_launch.py
-ros2 launch ros_chat_agent chat_agent.launch.py
+# Build the ROS 2 workspace
+colcon build
+source install/setup.bash
+
+# Launch the ROS 2 nodes
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch rh8d_controller_python position_controller.launch.py; exec bash" --hold
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch rh8d_hw rh8d_hw_combined.launch.py; exec bash" --hold
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch rh8d_ros2 rh8d_col.launch.py; exec bash" --hold
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch ros_sign_language_recognition recognizer_launch.py; exec bash" --hold
+gnome-terminal -- bash -c "source install/setup.bash; ros2 launch ros_chat_agent chat_agent.launch.py; exec bash" --hold
--- launch_gui.sh
+++ launch_gui.sh
@@ -1,8 +1,10 @@
-# 1. Build the ROS 2 workspace:
-colcon build
-. install/setup.bash
+#!/bin/bash
 
-# 2. Launch 
-ros2 launch rh8d_hw rh8d_hw_combined.launch.py
-ros2 launch rh8d_ros2 rh8d_col.launch.py
-ros2 launch rh8d_controller_python controller_gui.launch.py
+# Build the ROS 2 workspace
+colcon build
+source install/setup.bash
+
+# Launch the ROS 2 nodes
+gnome-terminal -- bash -c "ros2 launch rh8d_hw rh8d_hw_combined.launch.py; exec bash" --hold
+gnome-terminal -- bash -c "ros2 launch rh8d_ros2 rh8d_col.launch.py; exec bash" --hold
+gnome-terminal -- bash -c "ros2 launch rh8d_controller_python controller_gui.launch.py; exec bash" --hold
